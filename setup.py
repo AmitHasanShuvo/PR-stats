417435480c445988e7f923379e83fc6d5a1c7af9
AmitHasanShuvo <kaziamithasan89@gmail.com>
2023-01-06 04:39:54 -0500

    added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='pr_stats',
-    version='0.0.1',
+    version='0.0.2',
     author="Kazi Amit Hasan",
     author_email="kaziamithasan89@gmail.com",
     description="This module brings different stats about pull requests using GitHub API.",
